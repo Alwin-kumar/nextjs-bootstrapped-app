b113148979ef5e45e8542e62170ba33d4c975f31
Alwin Kumar S <alwin.se2k6@gmail.com>
2025-09-17 18:05:38 +0530

    Add next.config.js 1758112539109

---
+++ next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        pathname: '/photos/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        pathname: '/**',
+      },
+    ],
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', 'prisma'],
+  },
+}
+
+module.exports = nextConfig
